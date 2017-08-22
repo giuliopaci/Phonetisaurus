0a869bbdcc9c7cbefe9fb1887077a0e51fde6c2f
Giulio Paci <giuliopaci@gmail.com>
2017-08-23 00:58:21 +0200

    Made setup.py almost usable for out-of-tree compilation.

--- python/setup.py
+++ python/setup.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python
 from setuptools import setup, Extension
-import os
+from setuptools.command.egg_info import egg_info
+import os, sys
+
+class EggInfoCommand(egg_info):
+    def run(self):
+        if "build" in self.distribution.command_obj:
+            build_command = self.distribution.command_obj["build"]
+            self.egg_base = build_command.build_base
+            self.egg_info = os.path.join(self.egg_base, os.path.basename(self.egg_info))
+        egg_info.run(self)
+
+SRC_PATH = os.path.dirname(os.path.dirname(sys.argv[0]))
 
 phonetisaurusmodule = Extension(
     'Phonetisaurus',
-    include_dirs=['/usr/local/include', '../src'],
-    libraries=['c++'],
+    include_dirs=[os.path.join(SRC_PATH,'src')],
+    libraries=['fst', 'fstfar', 'fstscript'],
     library_dirs=[os.path.abspath('./build')],
-    sources=['test.cc'],
+    sources=['Phonetisaurus-binding.cc', os.path.join(SRC_PATH,'src/lib/util.cc')],
     language='c++',
     extra_compile_args=['-std=c++11', '-v'],
 )
@@ -23,6 +34,8 @@ setup (
     author_email = 'josef.robert.novak@gmail.com',
     license      = 'BSD',
     packages     = ['phonetisaurus'],
+    cmdclass={"egg_info" : EggInfoCommand},
+    package_dir={"" : os.path.join(SRC_PATH,"python")},
     ext_modules  = [phonetisaurusmodule],
     include_package_data = True,
     install_requires = ["argparse", "bottle"],
